eb79b8c6219ce078add9ba587233cb849426fe1c
Gabriel Peyré <gabriel.peyre@ens.fr>
2017-01-10 18:45:56 +0100

    update spectral density

--- code/test_spectral_densities.m
+++ code/test_spectral_densities.m
@@ -6,23 +6,35 @@ addpath('toolbox/');
 addpath('toolbox_quantum/');
 addpath('data/textures/');
 
-name = 'stonewall';
 name = 'grayfabric';
+name = 'fabric';
+name = 'stonewall';
+
+estimator_type = 'window';
+estimator_type = 'periodic';
 
 f = load_image(name);
+f = f/255;
 n0 = size(f,1);
 
 % window for estimation
-n = 64;
+n = 128*2;
 w = sin( pi*(0:n-1)'/n ).^2; w = w*w';
 % number of extracted window
-q = 4000;
+q = 200;
 
 T = zeros(3,3,n,n);
 for it=1:q
     progressbar(it,q);
     a = 1+floor(rand(2,1)*(n0-n));
-    h = repmat(w, [1 1 3]) .* f(a(1):a(1)+n-1, a(2):a(2)+n-1,:);
+    F = f(a(1):a(1)+n-1, a(2):a(2)+n-1,:); 
+    switch estimator_type
+        case 'window'
+            h = repmat(w, [1 1 3]) .* F;
+        case 'periodic'
+            h = periodic_comp(F);
+    end
+    h = h - repmat( mean(mean(F,1),2), [n n 1] );
     H = fft2(h);
     for i=1:3
         for j=1:3
@@ -48,7 +60,7 @@ B = ( S(:,:,2)-S(:,:,1) ) ./ ( S(:,:,2)+S(:,:,1) );
 img = @(x)imagesc(fftshift(x));
 clf; 
 subplot(2,2,1);
-img(log(S(:,:,3))); axis image; axis off; colorbar;
+img(log(S(:,:,3)+1e-10)); axis image; axis off; colorbar;
 title('log(Amplitude)');
 subplot(2,2,2);
 img(A); axis image; axis off; colorbar;
@@ -61,4 +73,26 @@ a = U(:,1,:,:);
 imageplot( squeeze(a(1,:,:,:) ) );
 title('Orientation');
 colormap jet(256); 
+
+
+% compute sqrtm of the covariance
+warning off;
+for i=1:n
+    for j=1:n
+        Ts(:,:,i,j) = sqrtm(T(:,:,i,j));
+    end
+end
+warning on;
+
+% synthesize by convolving with a noise
+W = randn(n)/n;
+W = W-mean(W(:))+1/n^2;
+W = fft2(W);
+%
+g = tensor_mult(Ts, reshape(repmat(W, [3 1]), [3 1 n n]));
+g = permute(squeeze(g), [2 3 1]);
+g = real( ifft2(g) );
+g = g + repmat( mean(mean(f,1),2), [n n 1] );
+clf; image(g); axis image; axis off;
+
 
---
+++ code/toolbox/periodic_comp.m
@@ -0,0 +1,34 @@
+function p = periodic_comp(h)
+
+% compute the periodic component of an image
+%
+%   p = periodic_comp(h);
+%
+%   Copyright (c) 2016 Gabriel Peyre
+
+[m, n, dm] = size(h);
+
+p = zeros(size(h));
+for d = 1:dm
+    Hhat = intLaplace(h(:,:,d));
+    FHhat = fft2(Hhat);
+    
+    [X Y] = meshgrid(0:n-1, 0:m-1);
+    FPhat = FHhat./(4 -2.*cos(2.*X*pi/n) -2.*cos(2.*Y*pi/m));
+    FPhat(1,1) = sum(sum(h(:,:,d)));
+    
+    p(:,:,d) = real(ifft2(FPhat));
+end
+
+end
+
+function p = intLaplace(h)
+% Compute the discrete Laplacian in the interior of the image domain
+Hext = [0, h(1,:), 0; h(:,1), h, h(:,end); 0, h(end,:), 0;];
+
+p = 4.*Hext - ( circshift(Hext,[0 1]) + circshift(Hext,[1 0]) +...
+    circshift(Hext,[-1 0]) + circshift(Hext,[0 -1]) );
+
+p = p(2:end-1, 2:end-1);
+
+end
